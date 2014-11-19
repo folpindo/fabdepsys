d778b34115519cfef2cfa43b841fe9adf1b039d1
Faustino Olpindo Jr <folpindo@perfectfitgroup.local@phmdlpc0770.perfectfitgroup.local>
2014-11-19 18:50:40 +0800

    add initial files

---
+++ Fabdepsys/Project/Service/Request.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Fabdepsys\Project\Service;
+
+/**
+  {
+
+  "server_name":<>,
+
+  "role":<>,
+
+  "project_id":<>,
+
+  "project_name":<>,
+
+  "package_name":<>,
+
+  "process_stage":"build",
+
+  "environment":"staging|production",
+
+  "status":"started|completed|failed",
+
+  "project_version":"1.0.0"
+
+  }
+
+ */
+class Request {
+    
+}
